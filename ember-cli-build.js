4164eff4e6a3df5183c033c39bded3d7ff883d9d
e0 <shan.huang@me.com>
2016-11-28 14:40:59 +0100

    add css moduels and skeleton

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +18,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('vendor/skeleton/css/normalize.css');
+  app.import('vendor/skeleton/css/skeleton.css');
+  app.import('app/styles/app.css');
 
   return app.toTree();
 };
