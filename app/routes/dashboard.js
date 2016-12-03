ddbdc05e7afc3601e2e2af57fa57a4c426d18328
e0 <shan.huang@me.com>
2016-12-03 19:41:19 +0100

    Start mocking data

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
+let models = [{
+  id: 'users',
+  name: 'Users'
+}, {
+  id: 'rabbits',
+  name: 'Rabbits'
+}, {
+  id: 'hunters',
+  name: 'Hunters'
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return models;
+  }
 });
